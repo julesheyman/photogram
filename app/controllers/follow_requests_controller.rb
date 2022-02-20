3728b7372b3b0062c26deb24a3cd52a777cd1624
anonymous anonymous <julia.heyman@kellogg.northwestern.edu>
2022-02-20 21:50:59 +0000

    Remove controller comments

--- app/controllers/follow_requests_controller.rb
+++ app/controllers/follow_requests_controller.rb
@@ -1,24 +1,19 @@
 class FollowRequestsController < ApplicationController
   before_action :set_follow_request, only: %i[show edit update destroy]
 
-  # GET /follow_requests
   def index
     @q = FollowRequest.ransack(params[:q])
     @follow_requests = @q.result(distinct: true).page(params[:page]).per(10)
   end
 
-  # GET /follow_requests/1
   def show; end
 
-  # GET /follow_requests/new
   def new
     @follow_request = FollowRequest.new
   end
 
-  # GET /follow_requests/1/edit
   def edit; end
 
-  # POST /follow_requests
   def create
     @follow_request = FollowRequest.new(follow_request_params)
 
@@ -30,7 +25,6 @@ class FollowRequestsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /follow_requests/1
   def update
     if @follow_request.update(follow_request_params)
       redirect_to @follow_request,
@@ -40,7 +34,6 @@ class FollowRequestsController < ApplicationController
     end
   end
 
-  # DELETE /follow_requests/1
   def destroy
     @follow_request.destroy
     redirect_to follow_requests_url,
@@ -49,12 +42,10 @@ class FollowRequestsController < ApplicationController
 
   private
 
-  # Use callbacks to share common setup or constraints between actions.
   def set_follow_request
     @follow_request = FollowRequest.find(params[:id])
   end
 
-  # Only allow a trusted parameter "white list" through.
   def follow_request_params
     params.require(:follow_request).permit(:sender_id, :recipient_id)
   end
