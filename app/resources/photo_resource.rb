e1cefe3d8754d30609e73cb5b312a838648fd2a9
anonymous anonymous <julia.heyman@kellogg.northwestern.edu>
2022-02-20 21:50:14 +0000

    Generate graphiti resources for photos

---
+++ app/resources/photo_resource.rb
@@ -0,0 +1,14 @@
+class PhotoResource < ApplicationResource
+  attribute :id, :integer, writable: false
+  attribute :created_at, :datetime, writable: false
+  attribute :updated_at, :datetime, writable: false
+  attribute :date, :string
+  attribute :caption, :string
+  attribute :user_id, :integer
+  attribute :image, :string
+
+  # Direct associations
+
+  # Indirect associations
+
+end
